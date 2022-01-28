10edfcd8da230e7b366858dfbeea7ffa5a3daf8b
minghaochen <mh_chen@zju.edu.cn>
2022-01-28 16:56:14 +0800

    PR

---
+++ PersonalRank/utlis.py
@@ -0,0 +1,141 @@
+import random
+import time
+import pandas as pd
+import math
+
+# 定义装饰器，监控运行时间
+def timmer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        res = func(*args, **kwargs)
+        stop_time = time.time()
+        print('Func %s, run time: %s' % (func.__name__, stop_time - start_time))
+        return res
+    return wrapper
+
+
+class Dataset():
+
+    def __init__(self, fp):
+        self.data = self.loadData(fp)
+
+    @timmer
+    def loadData(self, fp):
+        data = pd.read_csv(fp)
+        return list(zip(data.userId, data.movieId))
+
+    @timmer
+    def splitData(self, M, k, seed=1):
+        '''
+        :params: data, 加载的所有(user, item)数据条目
+        :params: M, 划分的数目，最后需要取M折的平均
+        :params: k, 本次是第几次划分，k~[0, M)
+        :params: seed, random的种子数，对于不同的k应设置成一样的
+        :return: train, test
+        '''
+        train, test = [], []
+        random.seed(seed)
+        for user, item in self.data:
+            if random.randint(0, M - 1) == k:
+                test.append((user, item))
+            else:
+                train.append((user, item))
+
+        # 处理成字典的形式，user->set(items)
+        def convert_dict(data):
+            data_dict = {}
+            for user, item in data:
+                if user not in data_dict:
+                    data_dict[user] = set()
+                data_dict[user].add(item)
+            data_dict = {k: list(data_dict[k]) for k in data_dict}
+            return data_dict
+
+        return convert_dict(train), convert_dict(test)
+
+
+
+
+# 评价指标 precision、recall、coverage、popularity
+class Metric():
+
+    def __init__(self, train, test, GetRecommendation):
+        '''
+        :params: train, 训练数据
+        :params: test, 测试数据
+        :params: GetRecommendation, 为某个用户获取推荐物品的接口函数
+        '''
+        self.train = train
+        self.test = test
+        self.GetRecommendation = GetRecommendation
+        self.recs = self.getRec()
+
+    # 为test中的每个用户进行推荐
+    def getRec(self):
+        recs = {}
+        for user in self.test:
+            rank = self.GetRecommendation(user)
+            recs[user] = rank
+        return recs
+
+    # 定义精确率指标计算方式
+    def precision(self):
+        all, hit = 0, 0
+        for user in self.test:
+            test_items = set(self.test[user])
+            rank = self.recs[user]
+            for item, score in rank:
+                if item in test_items:
+                    hit += 1
+            all += len(rank)
+        return round(hit / all * 100, 2)
+
+    # 定义召回率指标计算方式
+    def recall(self):
+        all, hit = 0, 0
+        for user in self.test:
+            test_items = set(self.test[user])
+            rank = self.recs[user]
+            for item, score in rank:
+                if item in test_items:
+                    hit += 1
+            all += len(test_items)
+        return round(hit / all * 100, 2)
+
+    # 定义覆盖率指标计算方式
+    def coverage(self):
+        all_item, recom_item = set(), set()
+        for user in self.test:
+            for item in self.train[user]:
+                all_item.add(item)
+            rank = self.recs[user]
+            for item, score in rank:
+                recom_item.add(item)
+        return round(len(recom_item) / len(all_item) * 100, 2)
+
+    # 定义新颖度度指标计算方式
+    def popularity(self):
+        # 计算物品的流行度
+        item_pop = {}
+        for user in self.train:
+            for item in self.train[user]:
+                if item not in item_pop:
+                    item_pop[item] = 0
+                item_pop[item] += 1
+
+        num, pop = 0, 0
+        for user in self.test:
+            rank = self.recs[user]
+            for item, score in rank:
+                # 取对数，防止因长尾问题带来的被流行物品所主导
+                pop += math.log(1 + item_pop[item])
+                num += 1
+        return round(pop / num, 6)
+
+    def eval(self):
+        metric = {'Precision': self.precision(),
+                  'Recall': self.recall(),
+                  'Coverage': self.coverage(),
+                  'Popularity': self.popularity()}
+        print('Metric:', metric)
+        return metric
